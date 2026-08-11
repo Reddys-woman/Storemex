d05bad571e0a476fa303bb609796521726680480
Yams <yams.bti@gmail.com>
2026-08-11 17:24:45 +0530

    Changed Gemini Model

--- storemex/backend/ai/scanner.js
+++ storemex/backend/ai/scanner.js
@@ -43,7 +43,7 @@ async function scanImage(imageInput) {
     }
 
     const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
+        model: "gemini-3.5-flash",
         contents: [
             {
                 inlineData: {
